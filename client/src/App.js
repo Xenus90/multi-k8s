0763e06674c548f4154d991b5ed073360b601548
Vladimir Greenberg <xenus90@gmail.com>
2020-08-20 00:00:14 +0300

    client error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
 	        <header className="App-header">
 		   		<img src={logo} className="App-logo" alt="logo" />
 		   		<h1 className="App-title">Fib Calculator</h1>
-		   		<Link to="/">Home></Link>
+		   		<Link to="/">Home</Link>
 		   		<Link to="/otherpage">Other page</Link>
 	        </header>
 	        <div>
